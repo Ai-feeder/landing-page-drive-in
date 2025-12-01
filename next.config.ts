80f863316cbe5bb4c7eb7087426cd99dd0f2aacc
Rian Septiawan <rianseptiawan@infitech.or.id>
2025-12-02 06:20:09 +0700

    deploy

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.tile.openstreetmap.org",
+      },
+      {
+        protocol: "https",
+        hostname: "cdnjs.cloudflare.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
